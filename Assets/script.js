29c9b59da7e8edebf4951e9bdb85234030b40a8e
likemike12345 <michael.manyonganise@gmail.com>
2023-01-31 22:47:12 +0000

    Completed code with README v1

---
+++ Assets/script.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+  console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+  /* This object stores tasks as strings so they can be retrieved them from local storage */
+  var tasksObj = JSON.parse(localStorage.getItem("tasksObj")) || {
+      "9": "",
+      "10": "",
+      "11": "",
+      "12": "",
+      "13": "",
+      "14": "",
+      "15": "",
+      "16": "",
+      "17": ""
+  };
+
+  /* This forloop retrives the tasks (taskObj) from the object and returns them to "textarea" on refresh of the page */
+  for (var i = 0; i < 9; i++) {
+      console.log(tasksObj[i + 9]);
+      console.log($(`#${i + 9}`).find(".description").text(loadTxt));
+      var loadTxt = tasksObj[i + 9];
+      $(`#${i + 9}`).find(".description").text(loadTxt);
+  };
+
+  /* The realtime time and day gathered from moment.js so that the app knows what day it is*/
+  var currentDay = $("#currentDay");
+
+  /* Gives the format in which the date should be shown */
+  currentDay.text(moment().format('dddd, MMMM Do YYYY'));
+
+  var currentHour = parseInt(moment().format('H'));
+
+  console.log(currentHour);
+
+  /* Stores tasks to local storage */
+  function storeTasks() {
+      localStorage.setItem("tasksObj", JSON.stringify(tasksObj));
+  };
+
+  /* This function styles the rows according to the time. This allows the app to figure out which tense the information is being entered relative to what time of the day it is from the moment.js */
+  function styleTasks() {
+
+      $(".row-info").each(function () {
+          var elementHour = parseInt(($(this).attr("id")));
+          
+          if (elementHour < currentHour) {
+              $(this).addClass("past");
+          }
+          else if (elementHour === currentHour) {
+              $(this).addClass("present");
+          }
+          else {
+              $(this).addClass("future");
+
+          };
+      });
+  };
+
+
+  console.log(tasksObj);
+
+  /* Click event to populate the tasksObj with text input from the textarea */ 
+  $("button").on("click", function () {
+      // get id
+      console.log($(this).parent().attr("id"));
+      var taskId = $(this).parent().attr("id");
+
+      // get textarea input
+      var textInput = $(this).parent().find("textarea");
+      textInput.val();
+      console.log(textInput.val());
+
+      // populate the obj in the background
+      console.log("my tasks: ", tasksObj);
+
+      tasksObj[taskId] = textInput.val();
+
+      // trigger a save function 
+      storeTasks();
+  });
+
+  styleTasks();
+
+});
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Daily-Planner
-A simple calendar application that allows a user to save events for each hour of the day
+# Daily-Planner App
+
+## Description
+A simple calendar application that allows a user to save events for each hour of the working day (assuming they work a 9am-5pm schedule). The app will save tasks for the entire day and then get rid of the tasks the following day. The date at the top of the app automatically updates daily while linked to moment.js. The app itself tracks by the hour using moment.js.
+
+## Pictures
+
+![Daily planner picture](./images/Daily%20planner%20example.png)
+
+## Installments
+Visual Studio code
+Moment.js - https://momentjs.com/ allows the app to keep up to date
+Font awesome - https://fontawesome.com/icons for the icons used in the calender app
+
+## Credits
+https://momentjs.com/guides/ - A guide used to help understand how to use the library moment.js
+
+
